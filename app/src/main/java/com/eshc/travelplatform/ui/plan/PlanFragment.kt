21b67c38c81c68c0d3343a6678d8a83e108394e9
Jung SeokJoon <eshc.joon@gmail.com>
2022-01-19 12:52:04 +0900

    add tablayout in planfragment

--- app/src/main/java/com/eshc/travelplatform/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/eshc/travelplatform/ui/plan/PlanFragment.kt
@@ -1,23 +1,25 @@
 package com.eshc.travelplatform.ui.plan
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eshc.travelplatform.R
 import com.eshc.travelplatform.adapter.DestinationAdapter
 import com.eshc.travelplatform.data.plan.Destination
 import com.eshc.travelplatform.databinding.FragmentPlanBinding
+import com.eshc.travelplatform.databinding.FragmentPlanBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.tabs.TabLayout
 
 
-class PlanFragment : BottomSheetDialogFragment() {
+class PlanFragment : Fragment() {
     private lateinit var binding: FragmentPlanBinding
     private val adapter by lazy { DestinationAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,43 +37,10 @@ class PlanFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (dialog as BottomSheetDialog).behavior.apply {
-            isFitToContents = true
-            state = BottomSheetBehavior.STATE_EXPANDED
-            skipCollapsed = true
-        }
-        val next = binding.temp
-        val previous1 = binding.temp1
-        val recyclerView = binding.rvDestination
-        val locationLayout = binding.llLocation
-        val calendarLayout = binding.llCalendar
-        initRecyclerView(recyclerView)
-        adapter.replaceAll(listOf(
-            Destination(
-            "서울"),Destination("부산"),Destination("대구"),Destination("대전"),Destination("강릉"),Destination("속초"),Destination(
-                "제주"),Destination("김해"),Destination("목포"),Destination("전주"),Destination("춘천"),Destination("진주"),Destination(
-                "광주"),Destination("인천"),Destination("가평"),Destination("경주"),Destination("울산"),Destination("천안")))
 
-        next.setOnClickListener {
-            val offsetFromTop = 200
-            (dialog as BottomSheetDialog).behavior.apply {
-                //isFitToContents = false
-                state = BottomSheetBehavior.STATE_COLLAPSED
-                peekHeight = 1600
-                    //expandedOffset = offsetFromTop
-            }
-            locationLayout.visibility = View.GONE
-            calendarLayout.visibility = View.VISIBLE
-        }
-        previous1.setOnClickListener {
-            (dialog as BottomSheetDialog).behavior.apply {
-                state = BottomSheetBehavior.STATE_EXPANDED
-                isFitToContents = true
+        val tabLayout = binding.tablayout
 
-            }
-            locationLayout.visibility = View.VISIBLE
-            calendarLayout.visibility = View.GONE
-        }
+        initTabLayout(tabLayout)
     }
     private fun initRecyclerView(rv:RecyclerView){
         val lm = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
@@ -79,5 +48,12 @@ class PlanFragment : BottomSheetDialogFragment() {
 
         rv.layoutManager = lm
         rv.adapter = adapter
+    }
+    private fun initTabLayout(tabLayout: TabLayout){
+        val tabNames = listOf("지도로 보기","목록으로 보기")
+        for(tabName in tabNames){
+            val newTab = tabLayout.newTab().setText(tabName)
+            tabLayout.addTab(newTab)
+        }
     }
 }
