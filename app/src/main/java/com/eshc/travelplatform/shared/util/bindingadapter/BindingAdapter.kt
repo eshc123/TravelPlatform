7cf9b057d16389a1b925a0b9090f166469e91196
Jung SeokJoon <eshc.joon@gmail.com>
2022-01-24 23:19:48 +0900

    refactor bindingadapter

--- app/src/main/java/com/eshc/travelplatform/shared/util/bindingadapter/BindingAdapter.kt
+++ app/src/main/java/com/eshc/travelplatform/shared/util/bindingadapter/BindingAdapter.kt
@@ -1,4 +1,21 @@
 package com.eshc.travelplatform.shared.util.bindingadapter
 
-class BindingAdapter {
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.eshc.travelplatform.R
+
+object BindingAdapter {
+
+    @BindingAdapter("coverSrc")
+    @JvmStatic fun setCoverSrc(view: AppCompatImageView, resource: String?) {
+        when (resource) {
+            null ->  Glide.with(view.context).load(R.drawable.background_corner_round).into(view)
+            "" -> Glide.with(view.context).load(R.drawable.background_corner_round).into(view)
+            else -> Glide.with(view.context).load(resource).centerCrop().placeholder(R.drawable.background_corner_round)
+                .into(view)
+        }
+
+        view.requestLayout()
+    }
 }
