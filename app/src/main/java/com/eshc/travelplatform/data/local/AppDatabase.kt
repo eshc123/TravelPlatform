9c5e08973a52965feee94be00b9c193a6875980a
Jung SeokJoon <eshc.joon@gmail.com>
2022-02-23 23:39:32 +0900

    add database

---
+++ app/src/main/java/com/eshc/travelplatform/data/local/AppDatabase.kt
@@ -0,0 +1,42 @@
+package com.eshc.travelplatform.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Database(entities = [],version = 1,exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    private class AppDatabaseCallBack(
+        private val scope : CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                }
+            }
+        }
+    }
+    companion object {
+        @Volatile
+        private var INSTANCE : AppDatabase? = null
+
+        fun getDatabase(context: Context, applicationScope: CoroutineScope) : AppDatabase{
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .addCallback(AppDatabaseCallBack(scope = applicationScope))
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
