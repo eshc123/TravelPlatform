47625b2116644e03352ef1546b323aa7e548f49e
Jung SeokJoon <eshc.joon@gmail.com>
2022-06-13 13:20:36 +0900

    #13 add myschedule adapter

--- app/src/main/java/com/eshc/travelplatform/ui/plan/MyscheduleFragment.kt
+++ app/src/main/java/com/eshc/travelplatform/ui/plan/MyscheduleFragment.kt
@@ -1,20 +1,23 @@
 package com.eshc.travelplatform.ui.plan
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.eshc.travelplatform.R
 import com.eshc.travelplatform.databinding.FragmentMyscheduleBinding
+import com.eshc.travelplatform.shared.util.adapter.MyscheduleAdapter
 
 class MyscheduleFragment : Fragment() {
 
     private val myscheduleViewModel: MyscheduleViewModel by viewModels<MyscheduleViewModel>()
     private lateinit var binding: FragmentMyscheduleBinding
-
+    val adapter = MyscheduleAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +31,11 @@ class MyscheduleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_myschedule, container, false)
+        binding.rvMyschedule.adapter = adapter
 
+        myscheduleViewModel.itineraries.observe(viewLifecycleOwner, Observer {
+            adapter.replaceAll(it)
+        })
 
 
         return binding.root
--- app/src/main/java/com/eshc/travelplatform/ui/plan/MyscheduleViewModel.kt
+++ app/src/main/java/com/eshc/travelplatform/ui/plan/MyscheduleViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.eshc.travelplatform.MainApplication
 import com.eshc.data.repository.SpotRepositoryLocalImpl
+import com.eshc.domain.model.Itinerary
 import com.eshc.domain.model.Spot
 import com.eshc.domain.model.User
 import com.eshc.domain.usecase.spot.GetKeepSpotsUseCase
@@ -14,4 +15,16 @@ import kotlinx.coroutines.launch
 class MyscheduleViewModel() :
     ViewModel() {
 
+    private val _itineraries = MutableLiveData<MutableList<Itinerary>>()
+    val itineraries: LiveData<MutableList<Itinerary>> = _itineraries
+
+    init {
+        _itineraries.postValue(
+            mutableListOf(
+                Itinerary("테스트1","테스트1","테스트","테스트",1),
+                Itinerary("테스트2","테스트2","테스트","테스트",2),
+                Itinerary("테스트3","테스트3","테스트","테스트",3),
+                Itinerary("테스트4","테스트4","테스트","테스트",4))
+        )
+    }
 }
