e9bf7edd7781320a463ea9eeb741fbfc0bf8ff76
Jung SeokJoon <eshc.joon@gmail.com>
2022-06-16 12:56:42 +0900

    #6 add edit dialog

--- app/src/main/java/com/eshc/travelplatform/ui/mypage/UserEditFragment.kt
+++ app/src/main/java/com/eshc/travelplatform/ui/mypage/UserEditFragment.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.eshc.travelplatform.R
 import com.eshc.travelplatform.databinding.FragmentMypageBinding
 import com.eshc.travelplatform.databinding.FragmentUserEditBinding
+import com.eshc.travelplatform.shared.util.DialogUtil
 
 
 class UserEditFragment : Fragment() {
@@ -28,13 +28,24 @@ class UserEditFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_user_edit, container, false)
         binding.fragment = this
 
+        binding.clNicknameEdit.setOnClickListener {
+            DialogUtil.showEditDialog(
+                context = requireContext(),
+                title = "닉네임 변경",
+                limit = 10,
+                ok = { Toast.makeText(requireContext(),"닉네임 변경 완료",Toast.LENGTH_SHORT).show() }
+                )
+        }
 
-
+        binding.clPasswordEdit.setOnClickListener {
+            DialogUtil.showEditDialog(requireContext(),
+                "비밀번호 변경",
+                limit = 10,
+                ok = { Toast.makeText(requireContext(),"비밀번호 변경 완료",Toast.LENGTH_SHORT).show()}
+                )
+        }
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
 
-    }
 }
